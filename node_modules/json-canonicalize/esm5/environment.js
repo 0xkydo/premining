c6835f900b959e76a9f50f01b81fc4905a04c934
Ubuntu <ubuntu@ip-172-31-38-148.us-west-2.compute.internal>
2023-02-13 06:08:06 +0000

    removed node_modules folder

--- node_modules/json-canonicalize/esm5/environment.js
+++
@@ -1,5 +0,0 @@
-/** * @internal */
-export var IS_DEV = process.env.NODE_ENV === 'development';
-/** * @internal */
-export var IS_PROD = process.env.NODE_ENV === 'production';
-//# sourceMappingURL=environment.js.map
